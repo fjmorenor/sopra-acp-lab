7a5805cb0b923d303a0f2e21baecd40d1f244167
Francisco <fmorenoreina@gmail.com>
2026-02-19 12:46:52 +0100

    Fix: Eliminada duplicidad de configuración del backend

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-# terraform/backend.tf
-terraform {
-  backend "gcs" {
-    bucket  = "tf-state-sopra-gc-lab" # El que creamos con gcloud storage
-    prefix  = "terraform/state" #he quitado la s
-  }
-}

ea0888aade609349a51b7704c22e09af2bc71aa2
Francisco <fmorenoreina@gmail.com>
2026-02-19 12:42:51 +0100

    corregio nombre bucket2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 # terraform/backend.tf
 terraform {
   backend "gcs" {
-    bucket  = "tf-state-sopra-gcp-lab" # El que creamos con gcloud storage
+    bucket  = "tf-state-sopra-gc-lab" # El que creamos con gcloud storage
     prefix  = "terraform/state" #he quitado la s
   }
 }
